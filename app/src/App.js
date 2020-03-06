debd6203137144d71a80d34d5c4bd5b79aec45f8
squarerobin <squarerobin.82@gmail.com>
2020-03-06 09:59:33 +0100

    styling

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { fetchJokes } from "./actions";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee, faHatCowboy } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCheckSquare,
+  faCoffee,
+  faHatCowboy
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-library.add(fab, faCheckSquare, faCoffee)
-
+library.add(fab, faCheckSquare, faCoffee);
 
 class App extends Component {
   componentDidMount() {
@@ -17,29 +20,27 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Jokes</h1>
+      <div className="app-wrapper">
+        <header>
+          <h1 className="title">Welcome to Jokes</h1>
         </header>
         {this.props.fetchingJokes ? (
-          <h3>We're fetching your jokes...<FontAwesomeIcon icon={faHatCowboy} /></h3>
+          <h3>
+            We're fetching your jokes...
+            <FontAwesomeIcon icon={faHatCowboy} />
+          </h3>
         ) : (
-          <div className="App-intro">
-          {this.props.jokes.map(joke => {
+          <div className="jokes card">
+            {this.props.jokes.map(joke => {
               return (
-                <div key={Date.now() * Math.random()} className="">
-                
-                <h2>{joke.type}</h2>
+                <div key={Date.now() * Math.random()} className="card is-child">
+                  <span className="tag is-danger ">{joke.type}</span>
 
-
-                <p>{joke.setup}</p>
-                <p>{joke.punchline}</p>
+                  <p className="setup">{joke.setup}</p>
+                  <p className="punchline">{joke.punchline}</p>
                 </div>
-              )
-              
-           
-            })} 
-            
+              );
+            })}
           </div>
         )}
         {this.props.error !== "" ? <h4>{this.props.error}</h4> : null}
